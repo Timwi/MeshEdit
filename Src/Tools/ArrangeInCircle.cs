448665d17884df19d78a6296716984a31361e303
Timwi <github@timwi.de>
2016-11-19 22:02:28 +0100

    Sort selection by angle.

--- Src/Tools/ArrangeInCircle.cs
+++ Src/Tools/ArrangeInCircle.cs
@@ -7,10 +7,17 @@ namespace MeshEdit
     {
         [Tool("Arrange selected vertices in a circle")]
         public static void ArrangeInCircle(
-            [ToolDouble("Radius of the circle?")] double radius,
-            [ToolDouble("Offset angle? (0 = first vertex is right of the center; vertices go clockwise)")] double offsetAngle)
+        //[ToolDouble("Radius of the circle?")] double radius,
+        //[ToolDouble("Offset angle? (0 = first vertex is right of the center; vertices go clockwise)")] double offsetAngle
+        )
         {
-            var center = new Pt(0, 0.150511, 0.1);
+            var center = new Pt(-0.7, 0.150511, 0.66);
+            var radius = .12 + .03;
+            var offsetAngle = -90;
+
+            //var center = new Pt(-0.54, 0.150511, 0.588);
+            //var radius = .12 + .03;
+            //var offsetAngle = 90;
 
             Program.Settings.Execute(new MoveVertices(Ut.NewArray(Program.Settings.SelectedVertices.Count, i => Tuple.Create(
                 Program.Settings.SelectedVertices[i],
---
+++ Src/Tools/SortSelection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace MeshEdit
+{
+    static partial class Tools
+    {
+        [Tool("Sort Selection")]
+        public static void SortSelection(
+            [ToolBool(
+                "• Which direction?",
+                "Clockwise",
+                "Counter-clockwise")]
+            bool cw)
+        {
+            var midPt = new Pt(
+                Program.Settings.SelectedVertices.Sum(p => p.X) / Program.Settings.SelectedVertices.Count,
+                Program.Settings.SelectedVertices.Sum(p => p.Y) / Program.Settings.SelectedVertices.Count,
+                Program.Settings.SelectedVertices.Sum(p => p.Z) / Program.Settings.SelectedVertices.Count);
+
+            Program.Settings.SelectedVertices.Sort((p1, p2) =>
+            {
+                var atan1 = Math.Atan2(p1.Z - midPt.Z, p1.X - midPt.X);
+                var atan2 = Math.Atan2(p2.Z - midPt.Z, p2.X - midPt.X);
+                return atan1.CompareTo(atan2) * (cw ? -1 : 1);
+            });
+        }
+    }
+}
