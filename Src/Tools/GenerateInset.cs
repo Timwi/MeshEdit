fe6b659d13e7125a21fc701643df736e97dfeb96
Timwi <github@timwi.de>
2016-11-26 17:33:00 +0100

    Ability to generate partial inset (not a closed loop)

--- Src/Tools/GenerateInset.cs
+++ Src/Tools/GenerateInset.cs
@@ -10,38 +10,37 @@ namespace MeshEdit
     static partial class Tools
     {
         [Tool("Generate beveled inset")]
-        public static void GenerateInset([ToolDouble("Bevel radius?")] double radius)
+        public static void GenerateInset([ToolDouble("Bevel radius?")] double radius, [ToolBool("Closed curve?", "Open", "Closed")] bool closed)
         {
-            if (Program.Settings.SelectedVertices.Count < 3)
+            if (Program.Settings.SelectedVertices.Count < (closed ? 3 : 2))
             {
-                DlgMessage.ShowInfo(@"Need at least 3 vertices for this tool.");
+                DlgMessage.ShowInfo(@"Need at least {0} vertices for {1} curve.".Fmt(closed ? 3 : 2, closed ? "a closed" : "an open"));
                 return;
             }
 
-            Program.Settings.Execute(new AddRemoveFaces(null, bevelFromCurve(Program.Settings.SelectedVertices, radius, 12).Select(f => new Face(f, false)).ToArray()));
+            Program.Settings.Execute(new AddRemoveFaces(null, bevelFromCurve(Program.Settings.SelectedVertices, radius, 12, closed).Select(f => new Face(f, false)).ToArray()));
         }
 
-        private static IEnumerable<Pt[]> bevelFromCurve(List<Pt> pts, double radius, int revSteps)
+        private static IEnumerable<VertexInfo[]> bevelFromCurve(List<Pt> pts, double radius, int revSteps, bool closed)
         {
-            return createMesh(true, false, pts
+            var nPts = pts
                 .Select((p, ix) => new
                 {
-                    AxisStart = p.Add(y: -radius),
-                    AxisEnd = p.Add(y: -radius) + (pts[(ix + 1) % pts.Count] - p) + (p - pts[(ix - 1 + pts.Count) % pts.Count]),
-                    Perpendicular = pts[ix]
+                    AxisStart = closed || ix != pts.Count - 1 ? p.Add(y: -radius) : pts[pts.Count - 2].Add(y: -radius),
+                    AxisEnd = closed || (ix != 0 && ix != pts.Count - 1) ? p.Add(y: -radius) + (pts[(ix + 1) % pts.Count] - p) + (p - pts[(ix - 1 + pts.Count) % pts.Count]) :
+                        ix == 0 ? pts[1].Add(y: -radius) : pts[pts.Count - 1].Add(y: -radius),
+                    Perpendicular = pts[ix],
+                    Center = pts[ix].Add(y: -radius)
                 })
                 .Select(inf => Enumerable.Range(0, revSteps)
                     .Select(i => -90 * i / (revSteps - 1))
-                    .Select(angle => inf.Perpendicular.Rotate(inf.AxisStart, inf.AxisEnd, angle))
+                    .Select(angle => new { Center = inf.Center, Rotated = inf.Perpendicular.Rotate(inf.AxisStart, inf.AxisEnd, angle) })
                     .ToArray())
-                .ToArray());
-        }
+                .ToArray();
 
-        private static IEnumerable<Pt[]> createMesh(bool closedX, bool closedY, Pt[][] pts)
-        {
-            return Enumerable.Range(0, pts.Length)
-                .SelectConsecutivePairs(closedX, (i1, i2) => Enumerable.Range(0, pts[0].Length)
-                    .SelectConsecutivePairs(closedY, (j1, j2) => new[] { pts[i1][j1], pts[i2][j1], pts[i2][j2], pts[i1][j2] }))
+            return Enumerable.Range(0, nPts.Length)
+                .SelectConsecutivePairs(closed, (i1, i2) => Enumerable.Range(0, nPts[0].Length)
+                    .SelectConsecutivePairs(false, (j1, j2) => new[] { nPts[i1][j1], nPts[i2][j1], nPts[i2][j2], nPts[i1][j2] }.Select(inf => new VertexInfo(inf.Rotated, null, inf.Rotated - inf.Center)).ToArray()))
                 .SelectMany(x => x);
         }
     }
