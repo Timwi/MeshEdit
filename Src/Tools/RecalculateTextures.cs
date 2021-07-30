567d4d350a9df7ac43b62660f561e21b20df0045
Timwi <github@timwi.de>
2021-07-30 15:52:56 +0200

    RecalculateTextures feature

--- Src/Tools/RecalculateTextures.cs
+++ Src/Tools/RecalculateTextures.cs
@@ -7,19 +7,26 @@ namespace MeshEdit
     static partial class Tools
     {
         [Tool("Recalculate texture coordinates from bounds")]
-        public static void RecalculateTextures()
+        public static void RecalculateTextures([ToolDouble("Multiply Y coordinate with factor?")] double yFactor)
         {
             if (Program.Settings.Faces.Count == 0)
                 return;
-            var minX = Program.Settings.Faces.Min(f => f.Vertices.Min(v => v.Location.X));
-            var minZ = Program.Settings.Faces.Min(f => f.Vertices.Min(v => v.Location.Z));
-            var maxX = Program.Settings.Faces.Max(f => f.Vertices.Max(v => v.Location.X));
-            var maxZ = Program.Settings.Faces.Max(f => f.Vertices.Max(v => v.Location.Z));
+
+            PointD translateCoords(Pt p) => new PointD(p.X + yFactor * p.Y, p.Z + yFactor * p.Y);
+
+            var minX = Program.Settings.Faces.Min(f => f.Vertices.Min(v => translateCoords(v.Location).X));
+            var minY = Program.Settings.Faces.Min(f => f.Vertices.Min(v => translateCoords(v.Location).Y));
+            var maxX = Program.Settings.Faces.Max(f => f.Vertices.Max(v => translateCoords(v.Location).X));
+            var maxY = Program.Settings.Faces.Max(f => f.Vertices.Max(v => translateCoords(v.Location).Y));
             Program.Settings.Execute(new ModifyTextureCoordinates(
                 Program.Settings.Faces
                     .Where(f => !f.Hidden)
                     .SelectMany(f => f.Vertices)
-                    .Select(v => Tuple.Create(v, v.Texture, new PointD((v.Location.X - minX) / (maxX - minX), (v.Location.Z - maxZ) / (minZ - maxZ))))
+                    .Select(v =>
+                    {
+                        var nv = translateCoords(v.Location);
+                        return Tuple.Create(v, v.Texture, new PointD((nv.X - minX) / (maxX - minX), (nv.Y - maxY) / (minY - maxY)));
+                    })
                     .ToArray()));
         }
     }
